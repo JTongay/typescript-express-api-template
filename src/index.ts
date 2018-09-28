d1f7cdae6c612668052e5034dc58431109f36da6
Joseph Tongay <dirk.tongay@gmail.com>
2018-09-27 20:51:14 -0500

    changing server config to check for ci and test env

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { Server } from './app';
 
 // create http server
 export const app = Server.bootstrap().app;
-export const server = (process.env.NODE_ENV !== 'test') ? app.listen(config.get('port')) : undefined;
+export const server = (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'ci') ? app.listen(config.get('port')) : undefined;
